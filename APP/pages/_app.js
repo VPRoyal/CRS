08f3af5c46963f4179ea3fe14b95e78f963636b0
VPRoyal <2019umt1500@mnit.ac.in>
2023-01-19 16:03:43 +0530

    wrap the container inside react router to use it

--- APP/pages/_app.js
+++ APP/pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/main.css'
-
-// This default export is required in a new `pages/_app.js` file.
+import { useEffect,useState } from 'react'
+import { BrowserRouter as Router } from "react-router-dom"
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [window,setWindow]=useState(false);
+  useEffect(() => setWindow(true), [])
+  return(
+    <div suppressHydrationWarning>
+   {window?<Router><Component {...pageProps} /></Router>:null}
+   </div>
+  )
 }
