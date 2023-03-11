40b48a5bad1cd2deb5210bb136bd6329fceea1ec
VPRoyal <2019umt1500@mnit.ac.in>
2023-03-12 00:51:22 +0530

    select issue resolved. Unique IDs generated by useId hools.

--- APP/components/Customs/Select.js
+++ APP/components/Customs/Select.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useId } from 'react'
 import styles from './select.module.css'
 export default function Select({ type, title, options, databack, reset }) {
     const [IsCheck, setCheck] = useState(false)
+    const id=useId()
     const [selection, setSelection] = useState(null)
     const handleClick = (e) => {
         var val = e.target.innerHTML
@@ -22,8 +23,8 @@ export default function Select({ type, title, options, databack, reset }) {
         <div className={styles.wrap} >
             <ul>
                 <label>{selection && type === 'A' ? selection : title}</label>
-                <input checked={IsCheck} onChange={(e) => { setCheck(e.target.checked) }} type="checkbox" id={styles.select} />
-                <label htmlFor={styles.select} className={styles.icons} >
+                <input checked={IsCheck} onChange={(e) => { setCheck(e.target.checked) }} type="checkbox" className={styles.select} id={id} />
+                <label htmlFor={id} className={styles.icons} >
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
                     </svg>
